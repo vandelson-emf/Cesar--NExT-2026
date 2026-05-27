f2a015e94dc02a5cc9a2f11a9936ffe32b331b1f
Vandelson Elias Monteiro Filho <vandelson.elias@gmail.com>
2026-05-27 20:44:47 -0300

    Aula sobre acesso a arquivos no python

---
+++ aula06/ler_arquivo.py
@@ -0,0 +1,17 @@
+arquivo = open ('aula06/arquivo_texto.txt', encoding='utf-8')
+
+# conteudo = arquivo.read()
+
+# print (conteudo)
+
+cadastro = {}
+
+with open ('aula06/arquivo_texto.txt', encoding='utf-8') as arquivo:
+
+    linhas = arquivo.readlines()
+    for linha in linhas:
+        linha = linha.strip()
+        codigo, nome, fone = linha.split(',')
+        cadastro[codigo] = (nome, fone)
+
+print (cadastro)
